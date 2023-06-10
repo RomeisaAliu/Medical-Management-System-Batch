e7d1179efc2ecb16b8f5e71b4448104b3847e997
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-06-10 12:02:22 +0200

    Listener

--- src/main/java/com/example/medicalBatch/listener/CustomStepExecutionListener.java
+++ src/main/java/com/example/medicalBatch/listener/CustomStepExecutionListener.java
@@ -1,35 +1,38 @@
 package com.example.medicalBatch.listener;
 
-
-import lombok.RequiredArgsConstructor;
+import com.example.medicalmanagement.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class CustomStepExecutionListener implements StepExecutionListener {
-    private final JdbcTemplate jdbcTemplate;
-    private static final String DELETE_QUERY = "DELETE FROM users ";
+
+    private final UserService userService;
+
+    @Autowired
+    public CustomStepExecutionListener(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
-        log.info("delete all old records from");
-        int deletedRows = jdbcTemplate.update(DELETE_QUERY);
-        log.info("Before Step we have deleted : {} records",deletedRows);
+        log.info("Deleting all old records from the 'users' table");
+        userService.deleteAllUsers();
+        log.info("All users have been deleted before the step");
     }
 
     @Override
     public ExitStatus afterStep(StepExecution stepExecution) {
-        if (ExitStatus.COMPLETED.equals(stepExecution.getExitStatus())){
+        if (ExitStatus.COMPLETED.equals(stepExecution.getExitStatus())) {
             log.info("The step has finished with status: {}", stepExecution.getExitStatus());
-            return stepExecution.getExitStatus();
+        } else {
+            log.info("Something bad has happened after the step: {}", stepExecution.getExitStatus());
         }
-        log.info("Something bad has happened after step: {}", stepExecution.getExitStatus());
         return stepExecution.getExitStatus();
     }
 }
