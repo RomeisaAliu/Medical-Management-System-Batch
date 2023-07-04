cc08e5db42571217dc9640ffb241819eeed70b18
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-07-04 16:43:51 +0200

    configuration spring mail

--- src/main/java/com/example/medicalBatch/MedicalBatchApplication.java
+++ src/main/java/com/example/medicalBatch/MedicalBatchApplication.java
@@ -1,10 +1,14 @@
 package com.example.medicalBatch;
 
+
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 @EnableBatchProcessing
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.medicalmanagement.dto","com.example.medicalmanagement.repository"})
 public class MedicalBatchApplication {
 
 	public static void main(String[] args) {
